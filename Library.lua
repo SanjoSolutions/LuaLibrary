c3bcd0ca58d7d05e4b280969162bb1f7fb88b642
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-12-22 18:32:57 +0100

    Some semantic versioning support
    and support for the case where multiple add-ons load a version of "Library".

--- Library.lua
+++ Library.lua
@@ -1,23 +1,62 @@
 Library = Library or {}
-local addOnName, AddOn = ...
+
 local _ = {}
 
-_.libraries = {}
+if not Library.libraries then
+  Library.libraries = {}
+end
 
 function Library.register(name, version, library)
 	if not _.libraries[name] then
     _.libraries[name] = {}
   end
-  if not _.libraries[name][version] then
-    _.libraries[name][version] = library
+  local major, minor, patch = _.parseSemanticVersion(version)
+  if not _.libraries[name][major] then
+    _.libraries[name][major] = {}
+  end
+  if not _.libraries[name][major][minor] then
+    _.libraries[name][major][minor] = {}
+  end
+  if not _.libraries[name][major][minor][patch] then
+    _.libraries[name][major][minor][patch] = library
+  end
+  if (
+    not _.libraries[name][major].highest or
+      minor > _.libraries[name][major].highest.version.minor or
+      (minor == _.libraries[name][major].highest.version.minor and patch > _.libraries[name][major].highest.version.patch)
+  ) then
+    _.libraries[name][major].highest = {
+      version = {
+        major = major,
+        minor = minor,
+        patch = patch
+      },
+      library = library
+    }
   end
 end
 
-function Library.retrieve(name, version)
+function Library.retrieve(name, versionConstraint)
   local versions = _.libraries[name]
   if versions then
-    local library = versions[version]
+    local library
+    if string.sub(versionConstraint, 1, 1) == '^' then
+      local version = string.sub(versionConstraint, 2)
+      local major = _.parseSemanticVersion(version)
+      library = _.retrieveHighestVersionWithMajor(versions, major)
+    else
+      library = versions[versionConstraint]
+    end
     return library
   end
   return nil
+end
+
+function _.parseSemanticVersion(semanticVersion)
+	local major, minor, patch = string.match(semanticVersion, '(%d+)%.(%d+)%.(%d+)')
+  return major, minor, patch
+end
+
+function _.retrieveHighestVersionWithMajor(versions, major)
+	return versions[major].highest.library
 end
